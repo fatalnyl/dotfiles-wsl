ced682f26355c0d0f1f7169a980ccffc67043926
tlpoh00 <dicky.ting.le.poh@intel.com>
2025-10-22 11:11:35 +0800

    added option to set unset SSH github proxy

--- proxyset.sh
+++ proxyset.sh
@@ -1,3 +1,18 @@
 http_proxy=http://proxy-chain.intel.com:911 https_proxy=http://proxy-chain.intel.com:912
 git config --global http.proxy http://proxy-chain.intel.com:912
 git config --global https.proxy http://proxy-chain.intel.com:912
+
+#!/bin/bash
+CONFIG="$HOME/.ssh/config"
+
+# Add proxy for github.com if not already present
+if ! grep -q '^Host github.com' "$CONFIG"; then
+  echo "Adding SSH proxy for github.com..."
+  cat >> "$CONFIG" <<'EOF'
+
+Host github.com
+   ProxyCommand connect -a none -S proxy-dmz.intel.com:1080 %h %p
+EOF
+else
+  echo "SSH entry for Github already exists."
+fi
--- proxyunset.sh
+++ proxyunset.sh
@@ -2,3 +2,16 @@ unset {http,https,ftp}_proxy
 unset {HTTP,HTTPS,FTP}_PROXY
 git config --global --unset http.proxy 
 git config --global --unset https.proxy
+
+#!/bin/bash
+CONFIG="$HOME/.ssh/config"
+TMP="$(mktemp)"
+
+echo "Removing SSH proxy for github.com..."
+awk '
+  $1=="Host" && $2=="github.com"{inblock=1;next}
+  inblock && /^Host /{inblock=0}
+  !inblock
+' "$CONFIG" > "$TMP"
+
+awk 'NF{print; blank=0} !NF && !blank{print ""; blank=1}' "$TMP" > "$CONFIG"
